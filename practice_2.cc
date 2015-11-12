2a9125ca69c6f0af19dc2a5e3c5f8dff23a708fd
haden9 <haden9@gmail.com>
2015-11-11 22:38:39 -0600

    Update practice_2 comments

--- practice_2.cc
+++ practice_2.cc
@@ -1,21 +1,23 @@
 #include <iostream>
 
 /*
- * This program receives two
- * integer params and sums them
- * to display the result
+ * This program receives two integer params and sums
+ * them to display their result
  */
 int main()
 {
-  int v1 = 0, v2 = 0, result = 0; //declare and initialize values
+  //declare and initialize values
+  int v1 = 0, v2 = 0, result = 0; 
   std::cout << "Enter the first number:" << std::endl;
   std::cin >> v1; //obtain the first value
   std::cout << "Enter the second number:" << std::endl;
   std::cin >> v2; //obtain the second value
   result = v1 + v2;
+  //display the sum result
   std::cout << "The sum of " << v1
             << " and " << v2
-            << " is " << result << std::endl; //display the sum result
+            << " is " << result
+            << std::endl;
   return 0;
 }
 
