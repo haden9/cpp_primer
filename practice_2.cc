dc2ed41189f5db641c6a26584e6eb25abc067109
haden9 <haden9@gmail.com>
2015-11-11 23:01:39 -0600

    Add practice_3

--- practice_2.cc
+++ practice_2.cc
@@ -6,14 +6,14 @@
  */
 int main()
 {
-  //declare and initialize values
+  // declare and initialize values
   int v1 = 0, v2 = 0, result = 0; 
   std::cout << "Enter the first number:" << std::endl;
-  std::cin >> v1; //obtain the first value
+  std::cin >> v1; // obtain the first value
   std::cout << "Enter the second number:" << std::endl;
-  std::cin >> v2; //obtain the second value
+  std::cin >> v2; // obtain the second value
   result = v1 + v2;
-  //display the sum result
+  // display the sum result
   std::cout << "The sum of " << v1
             << " and " << v2
             << " is " << result
---
+++ practice_3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+  int fromRange = 0, toRange = 0;
+
+  /*
+   * This program displays the range between two
+   * numbers that are provided
+   */
+  std::cout << "Enter a number: " << std::endl;
+  std::cin >> fromRange;
+  std::cout << "Enter a second number: " << std::endl;
+  std::cin >> toRange;
+
+  while(fromRange <= toRange) {
+    std::cout << fromRange ++ << std::endl;
+  }
+
+  return 0;
+}
